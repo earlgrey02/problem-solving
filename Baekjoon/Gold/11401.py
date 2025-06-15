eba10229cc0def0869a93b84ed60e2eda301144b
earlgrey02 <san06036@naver.com>
2025-06-15 20:10:45 +0900

    2025.06.15

---
+++ Baekjoon/Gold/11401.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+p = int(1e9 + 7)
+
+def factorial(x: int):
+    result = 1
+
+    for i in range(2, x + 1):
+        result = (result * i) % p
+
+    return result
+
+print((factorial(n) * pow(factorial(n - k) * factorial(k), p - 2, p)) % p)
---
+++ Baekjoon/Silver/1780.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+def check_count(matrix: list[list[int]]):
+    if all(i == (first := matrix[0][0]) for row in matrix for i in row):
+        counts[first] += 1
+    else:
+        n = len(matrix)
+        div = n // 3
+
+        for i in range(0, n, div):
+            for j in range(0, n, div):
+                sub_matrix = [row[j:j + div] for row in matrix[i:i + div]]
+                check_count(sub_matrix)
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+counts = {i: 0 for i in range(-1, 2)}
+
+check_count(matrix)
+
+print(*counts.values(), sep = '\n')
