5197f9328cfd41be19eac3e3c86377d0823c1ead
earlgrey02 <san06036@naver.com>
2025-08-05 15:54:20 +0900

    2025.08.05

---
+++ Baekjoon/Silver/11057.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[1 if i == 1 else 0 for _ in range(10)] for i in range(n + 1)]
+
+for i in range(2, n + 1):
+    for j in range(10):
+        dp[i][j] = sum(dp[i - 1][:j + 1]) % 10007
+
+print(sum(dp[n]) % 10007)
